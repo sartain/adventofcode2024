9a351854111e4343939b5a3f7a2ae54d29e2be1c
sartain <asartain59@gmail.com>
2024-12-07 11:34:59 +0000

    day1: add splitting of list of inputs

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,6 +1,20 @@
-fun splitNumbersGivenString(input: String): List<Int>{
+import kotlin.streams.toList
+
+fun splitNumbersGivenString(input: String): List<Int> {
     val splitInput = input.split("    ")
     return splitInput.stream()
         .map { e -> e.toInt() }
+        .toList()
+}
+
+fun getLeftSideGivenListOfNumbers(input: List<String>): List<Int> {
+    return input.stream()
+        .map { inputString -> splitNumbersGivenString(inputString)[0] }
+        .toList()
+}
+
+fun getRightSideGivenListOfNumbers(input: List<String>): List<Int> {
+    return input.stream()
+        .map { inputString -> splitNumbersGivenString(inputString)[1] }
         .toList()
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -11,4 +11,44 @@ class Day1Test {
         assertEquals(actual, expected)
     }
 
+    @Test
+    fun givenListOfInputsGetLeftSide() {
+        val input = listOf(
+            "1    2",
+            "2    5",
+            "6    8",
+            "9    5666",
+            "5655    5"
+        )
+        val expected = listOf(
+            1,
+            2,
+            6,
+            9,
+            5655
+        )
+        val actual = getLeftSideGivenListOfNumbers(input)
+        assertEquals(actual, expected)
+    }
+
+    @Test
+    fun givenListOfInputsGetRightSide() {
+        val input = listOf(
+            "1    2",
+            "2    5",
+            "6    8",
+            "9    5666",
+            "5655    5"
+        )
+        val expected = listOf(
+            2,
+            5,
+            8,
+            5666,
+            5
+        )
+        val actual = getRightSideGivenListOfNumbers(input)
+        assertEquals(actual, expected)
+    }
+
 }
